f0c932b0b9af4da56020c7a194111c5e76a59da0
hhx <hanxue_huang@163.com>
2021-10-28 18:57:30 +0800

    chore: Modify some details

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,13 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  __SERVICE__: '"https://testnet2.gxchain.org"',
+  network:{
+    blockchain: '"gxc"',
+    protocol: '"https"',
+    host: '"testnet.gxchain.org"',
+    port: 443,
+    chainId: '"c2af30ef9340ff81fd61654295e98a1ff04b23189748f86727d0b26b40bb0ff4"'
+  },
+    contractName: '"bjnvoting"',
 }
